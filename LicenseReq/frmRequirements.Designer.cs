b6cb42d1fa10b520e40d18354753286d56e80e2a
hacsr <Uu9oCztFJ6IqPzfXa2Wur!f8lqd0cAVcVTNJic!2y37E>
2022-01-29 18:52:20 -0800

    Add project files.

---
+++ LicenseReq/frmRequirements.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace LicenseReq
+{
+    partial class frmRequirements
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRequirements));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(628, 119);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(0, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(628, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SEND THE HARDWARE ID TO PORTASFTP@ALPHASCUTI.COM";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmRequirements
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 144);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(650, 200);
+            this.Name = "frmRequirements";
+            this.Text = "Required License Information";
+            this.Load += new System.EventHandler(this.frmRequirements_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ LicenseReq/frmRequirements.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LicenseReq
+{
+    public partial class frmRequirements : Form
+    {
+        public frmRequirements()
+        {
+            InitializeComponent();
+        }
+
+        private void GetInfo()
+        {
+
+            StringBuilder stringBuilder = new();
+
+            stringBuilder.AppendLine("HARDWARE ID = " + License.Status.GetHardwareID(true,true,true,true));
+
+
+            stringBuilder.AppendLine();
+
+
+            if (License.Status.Licensed)
+            {
+                for (int i = 0; i < License.Status.KeyValueList.Count; i++)
+                {
+                    stringBuilder.AppendLine("   License key: " + License.Status.KeyValueList.GetKey(i).ToString() + "; Value: " + License.Status.KeyValueList.GetByIndex(i).ToString());
+                }
+            }
+
+            this.textBox1.Text = stringBuilder.ToString();
+           
+        }
+
+
+        private void frmRequirements_Load(object sender, EventArgs e)
+        {
+            textBox1.ReadOnly = true;
+            GetInfo();
+        }
+    }
+}
